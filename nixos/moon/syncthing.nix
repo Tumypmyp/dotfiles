41e175824acd13ac9532a202e8155a8fa3693ac6
Timur Gainullin <gaynoullin@gmail.com>
2025-06-06 23:55:34 +0500

    feature: share dir with syncthing

--- nixos/moon/syncthing.nix
+++ nixos/moon/syncthing.nix
@@ -1,8 +1,8 @@
 { ... }:
 {  
-  age.secrets.syncthing = {
-    file = "/home/pandenko/dotfiles/nixos/secrets/syncthing.age";
-  };
+#  age.secrets.syncthing = {
+#    file = "/home/pandenko/dotfiles/nixos/secrets/syncthing.age";
+#  };
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
@@ -17,10 +17,12 @@
       folders = {
         "backups" = {
           path = "/home/pandenko/backups";
+          devices = [ "heart" "moon" ];
         };
       };
       devices = {
         "moon" = { id = "VLDXORB-JOJBJTZ-GQ23PN4-K4JSJ67-VSNWUCX-TBKB6F6-4KDWJSE-OUGSVQF"; };
+        "heart" = { id = "FL2RAAG-GV3WN46-5YTHYNV-7FMXJMZ-5MYYVXV-2R2KR6D-A6GOOJ7-FVUTXAY"; };
       };
     };
   };
