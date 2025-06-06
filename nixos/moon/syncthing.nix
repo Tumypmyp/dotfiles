036ed1a4ba780e54bcb79e729326c13d7524d47c
Timur Gainullin <gaynoullin@gmail.com>
2025-06-06 23:34:15 +0500

    feature: start syncting

--- nixos/moon/syncthing.nix
+++ nixos/moon/syncthing.nix
@@ -6,10 +6,22 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    user = "pandenko";
     guiAddress = "0.0.0.0:8384";
+    configDir = "/home/pandenko/.config/syncthing";
     settings.gui = {
       user = "pandenko";
       password = "pass";
+    };
+    settings = {
+      folders = {
+        "backups" = {
+          path = "/home/pandenko/backups";
+        };
+      };
+      devices = {
+        "moon" = { id = "VLDXORB-JOJBJTZ-GQ23PN4-K4JSJ67-VSNWUCX-TBKB6F6-4KDWJSE-OUGSVQF"; };
+      };
     };
   };
 }
