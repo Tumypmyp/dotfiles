d241b77387cfaf7d503fb28d57e649d447f6977b
Timur Gainullin <gaynoullin@gmail.com>
2025-06-08 18:18:03 +0500

    feature: add windows to syncthing

--- nixos/common/syncthing.nix
+++ nixos/common/syncthing.nix
@@ -22,7 +22,7 @@
         "docs, concepts" = {
           id = "gvqru-98eqc";
           path = "/home/pandenko/backups/docs";
-          devices = [ "heart" "moon" "tablet" "phone" ];
+          devices = [ "heart" "moon" "tablet" "phone" "windows" ];
         };
       };
 
@@ -31,6 +31,7 @@
         "heart" = { id = "FL2RAAG-GV3WN46-5YTHYNV-7FMXJMZ-5MYYVXV-2R2KR6D-A6GOOJ7-FVUTXAY"; };
         "tablet" = { id = "YDT72CO-K63SAP2-DTKYSCJ-HO2ILUZ-MAXSPAW-YOFSHN5-JIXOVN6-MDU5NAM"; };
         "phone" = { id = "HNYOF7D-XFC645Y-EB64VP3-AH7FVSN-ZI4MGB4-TXC676C-6OLWJSK-ENWDXAC"; };
+        "windows" = { id = "A25EUXY-MWZ7ZAY-3C3U4XT-SXV7NSN-MA5EE4J-ULSYDSA-34IV6DV-VWIQGAT"; };
       };
     };
   };
